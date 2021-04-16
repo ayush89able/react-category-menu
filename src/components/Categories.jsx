2a53a354c968c8c7d925e4be2b18edf6bbdf0cb5
ayush sharma <ayush89able@gmail.com>
2021-04-17 04:22:13 +0530

    more test

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
-import CategoryItem from './CategoryItem'
-import AddSubCategory from './AddSubCategory'
-import { addCategorySync, addSubCategory, deleteCategory, editCategoryFunction } from '../redux/action' 
-import { FaPlus, FaMinus,FaPlusSquare, FaMinusSquare, FaTrash, FaRegEdit } from "react-icons/fa";
+import { addCategorySync, addSubCategory, deleteCategory, editCategoryFunction } from '../redux/action'
+import { FaPlus, FaMinus, FaPlusSquare, FaMinusSquare, FaTrash, FaRegEdit } from "react-icons/fa";
 
 const Categories = () => {
 
-  const [ newCategory, setNewCategory ] = useState('')
-  const [ newSubCategory, setNewSubCategory ] = useState('')
-  const [ editCategory, setEditCategory ] = useState('')
-  const [ expandAll, setExpandAll ] = useState(false)
-  const [ expand, setExpand ] = useState(false)
-  const [ indexNumber, setIndexNumber ] = useState(null)
-  const [ editIndexNumber, setEditIndexNumber ] = useState(null)
+  const [newCategory, setNewCategory] = useState('')
+  const [newSubCategory, setNewSubCategory] = useState('')
+  const [editCategory, setEditCategory] = useState('')
+  const [expandAll, setExpandAll] = useState(false)
+  const [expand, setExpand] = useState(false)
+  const [indexNumber, setIndexNumber] = useState(null)
+  const [editIndexNumber, setEditIndexNumber] = useState(null)
 
   const state = useSelector(state => state);
   // console.log('state',state)
@@ -32,25 +30,25 @@ const Categories = () => {
     setNewCategory('')
   }
 
-  const showAddSubCategory = (index,item) => {
+  const showAddSubCategory = (index, item) => {
     setIndexNumber(index)
     setExpand(!expand)
   }
 
-  const showEditCategory = (index,item) => {
+  const showEditCategory = (index, item) => {
     setEditIndexNumber(index)
   }
 
-  const deleteCategoryHandler = (index,item) => {
+  const deleteCategoryHandler = (index, item) => {
     console.log('Deleteing Category')
-    console.log('index',index)
-    console.log('item',item)
+    console.log('index', index)
+    console.log('item', item)
     dispatch(deleteCategory(index))
   }
 
-  const addSubCategoryHandler = (index,item) => {
-    console.log('index',index)
-    console.log('item',item)
+  const addSubCategoryHandler = (index, item) => {
+    console.log('index', index)
+    console.log('item', item)
     let subCategoryLength = item.subCategory.length
     console.log(subCategoryLength)
     let payload = {
@@ -59,111 +57,111 @@ const Categories = () => {
       subCategory: []
     }
     let subCategory = item.subCategory.concat(payload);
-    console.log('subCategories',subCategory)
+    console.log('subCategories', subCategory)
     let payloadTwo = {
       ...item,
       subCategory
     }
-    console.log('payloadTwo',payloadTwo)
-    dispatch(addSubCategory(payloadTwo,index))
+    console.log('payloadTwo', payloadTwo)
+    dispatch(addSubCategory(payloadTwo, index))
     setIndexNumber(null)
     setNewSubCategory('')
   }
 
-  const editCategoryHandler = (index,item) => {
+  const editCategoryHandler = (index, item) => {
     console.log('editCategory', editCategory)
-    console.log('index',index)
-    console.log('item',item)
+    console.log('index', index)
+    console.log('item', item)
     let payload = {
       ...item,
       value: editCategory
     }
     console.log('payload edit Category', payload)
-    dispatch(editCategoryFunction(payload,index))
+    dispatch(editCategoryFunction(payload, index))
     setEditIndexNumber(null)
     setEditCategory('')
   }
 
   const saveHandler = () => {
-    localStorage.setItem('state',JSON.stringify(state))
+    localStorage.setItem('state', JSON.stringify(state))
   }
 
-  
-  return(
+
+  return (
     <div className="container">
-    <h1>{`${state.length} Categories Present`}</h1>
-     <div className="row" style={{marginBottom: '1rem'}}>
-     <div className="col">
-     </div>
-    <div className="col">
-    <button  onClick={() => setExpandAll(!expandAll)} className="btn btn-primary">{expandAll ? <FaMinusSquare/> : <FaPlusSquare />}</button>
-    </div>
-     <div className="col">
-    <button  onClick={saveHandler} className="btn btn-primary">Save to local</button>
-    </div>
-     <div className="col">
-    </div>
-    </div>
-    {state.map((item,index)=> {
-      return (
-       <div className="row" key={index} style={{marginBottom:'1em'}}>
+      <h1>{`${state.length} Categories Present`}</h1>
+      <div className="row" style={{ marginBottom: '1rem' }}>
         <div className="col">
-          {item.value}
-          {item.subCategory.length > 0 && expandAll && (
-          item.subCategory.map((subItem,index)=> {
-            return(
-               <div className="row" key={index} style={{marginLeft:'1em'}}>
-                <div className="col">
-                  <div>{subItem.value}</div>
+        </div>
+        <div className="col">
+          <button onClick={() => setExpandAll(!expandAll)} className="btn btn-primary">{expandAll ? <FaMinusSquare /> : <FaPlusSquare />}</button>
+        </div>
+        <div className="col">
+          <button onClick={saveHandler} className="btn btn-primary">Save to local</button>
+        </div>
+        <div className="col">
+        </div>
+      </div>
+      {state.map((item, index) => {
+        return (
+          <div className="row" key={index} style={{ marginBottom: '1em' }}>
+            <div className="col">
+              {item.value}
+              {item.subCategory.length > 0 && expandAll && (
+                item.subCategory.map((subItem, index) => {
+                  return (
+                    <div className="row" key={index} style={{ marginLeft: '1em' }}>
+                      <div className="col">
+                        <div>{subItem.value}</div>
+                      </div>
+                    </div>
+                  )
+                })
+              )}
+              {item.subCategory.length > 0 && indexNumber === index && expand && (
+                item.subCategory.map((subItem, index) => {
+                  return (
+                    <div className="row" key={index} style={{ marginLeft: '1em' }}>
+                      <div className="col">
+                        <div>{subItem.value}</div>
+                      </div>
+                    </div>
+                  )
+                })
+              )}
+            </div>
+            <div className="col">
+              <button id={index} onClick={() => { showAddSubCategory(index, item) }} className="btn btn-primary">{expand && indexNumber === index ? <FaMinus /> : <FaPlus />}</button>
+              {indexNumber === index && expand && (
+                <div className="row" style={{ margin: '1em 0' }}>
+                  <input type='text' value={newSubCategory} data-testid={`addSubCategoryInput-${index}`} placeholder='New Sub Category' onChange={(e) => setNewSubCategory(e.target.value)} />
+                  <button id={index} data-testid={`addSubCategoryButton-${index}`} onClick={() => { addSubCategoryHandler(index, item) }} className="btn btn-primary" style={{ margin: '0.5em 0' }}>Add Sub Category</button>
                 </div>
-              </div>
-            )
-          })
-        )}
-        {item.subCategory.length > 0 && indexNumber === index && expand && (
-          item.subCategory.map((subItem,index)=> {
-            return(
-               <div className="row" key={index} style={{marginLeft:'1em'}}>
-                <div className="col">
-                  <div>{subItem.value}</div>
+              )}
+            </div>
+            <div className="col">
+              <button id={index} data-testid={`deleteButton-${index}`} onClick={() => { deleteCategoryHandler(index, item) }} className="btn btn-primary"><FaTrash /></button>
+            </div>
+            <div className="col">
+              <button id={index} onClick={() => { showEditCategory(index, item) }} className="btn btn-primary"><FaRegEdit /></button>
+              {editIndexNumber === index && (
+                <div className="row" style={{ margin: '1em 0' }}>
+                  <input placeholder='Edited Category Name' data-testid={`editCategoryInput-${index}`} type='text' value={editCategory} onChange={(e) => setEditCategory(e.target.value)} />
+                  <button id={index} data-testid={`editCategoryButton-${index}`} onClick={() => { editCategoryHandler(index, item) }} className="btn btn-primary" style={{ margin: '0.5em 0' }}>Edit Category</button>
                 </div>
-              </div>
-            )
-          })
-        )}
+              )}
+            </div>
+          </div>
+        )
+      })}
+      <div className="row">
+        <div className="col">
+          <input type='text' placeholder='New Category' value={newCategory} onChange={(e) => setNewCategory(e.target.value)} />
         </div>
         <div className="col">
-          <button id={index} onClick={() => {showAddSubCategory(index,item)}} className="btn btn-primary">{expand && indexNumber === index? <FaMinus/> : <FaPlus />}</button>
-          {indexNumber === index && expand && (
-          <div className="row" style={{margin: '1em 0'}}>
-            <input type='text' value={newSubCategory} placeholder='New Sub Category' onChange={(e) => setNewSubCategory(e.target.value)} />
-            <button id={index} onClick={() => {addSubCategoryHandler(index,item)}} className="btn btn-primary" style={{margin: '0.5em 0'}}>Add Sub Category</button>
-          </div>
-          )}
+          <button data-testid='addCategoryButton' onClick={addCategoryHandler} className="btn btn-primary">Add Category</button>
         </div>
-        <div className="col">
-          <button id={index} onClick={() => {deleteCategoryHandler(index,item)}} className="btn btn-primary"><FaTrash /></button>
-        </div>
-         <div className="col">
-          <button id={index} onClick={() => {showEditCategory(index,item)}} className="btn btn-primary"><FaRegEdit/></button>
-           {editIndexNumber === index && (
-          <div className="row" style={{margin: '1em 0'}}>
-            <input placeholder='Edited Category Name' type='text' value={editCategory} onChange={(e) => setEditCategory(e.target.value)} />
-            <button id={index} onClick={() => {editCategoryHandler(index,item)}} className="btn btn-primary" style={{margin: '0.5em 0'}}>Edit Category</button>
-          </div>
-          )}
-        </div>
-        </div>
-      )
-    })}
-    <div className="row">
-    <div className="col">
-    <input type='text' placeholder='New Category' value={newCategory} onChange={(e)=> setNewCategory(e.target.value)}/>
-    </div>
-    <div className="col">
-    <button onClick={addCategoryHandler} className="btn btn-primary">Add Category</button>
-    </div>
-    </div>
+      </div>
     </div>
   )
 }
--- src/components/__tests__/Categories.spec.js
+++ src/components/__tests__/Categories.spec.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux'
 import Categories from '../Categories'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { deleteCategory } from '../../redux/action'
 
 import configureStore from 'redux-mock-store';
 
@@ -48,15 +51,45 @@ describe('Categories Component', () => {
         ];
 
         store = mockStore(initialState);
+        store.dispatch = jest.fn();
 
-        component = renderer.create(
+        // component = renderer.create(
+        //     <Provider store={store}>
+        //         <Categories />
+        //     </Provider>
+        // );
+        const { container } = render(
             <Provider store={store}>
                 <Categories />
             </Provider>
         );
+        component = container
     });
 
     it('should render with given state from Redux store', () => {
-        expect(component.toJSON()).toMatchSnapshot();
+        // expect(component.toJSON()).toMatchSnapshot();
+        expect(component.firstChild).toMatchSnapshot();
     });
+
+    it('should dispatch an action on add sub category button click', () => {
+        userEvent.click(screen.getByTestId('addSubCategoryButton-0'))
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+    });
+
+    it('should dispatch an action on edit category button click', () => {
+        userEvent.click(screen.getByTestId('editCategoryButton-0'))
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+    });
+
+    it('should dispatch an action on delete button click', () => {
+        userEvent.click(screen.getByTestId('deleteButton-0'))
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+    });
+
+    it('should dispatch an action on add category button click', () => {
+        userEvent.click(screen.getByTestId('addCategoryButton'))
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+    });
+
+
 })
